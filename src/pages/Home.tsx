b46a78c729813ab5da09983a68d173c18c4866cd
Erwan Soulas <erwan.soulas@gmail.com>
2024-06-17 23:13:13 +0200

    9th commit: start solution section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 import Header from "@/components/navigation/Header";
 import Wrapper from "@/components/Wrapper";
 import Smartphone from "@/assets/images/smartphone2.png";
+import Bus from "@/assets/images/bus.png";
+import Car from "@/assets/images/car.png";
+import Truck from "@/assets/images/truck.png";
 
 export default function Home() {
     return (
@@ -15,83 +20,33 @@ export default function Home() {
                         <div className="text-center">
                             <h1 className="text-5xl font-bold">Gest-Flotte</h1>
                             <p className="text-2xl mt-4 font-semibold">
-                                <span className="font-bold text-white text-3xl">LA</span> solution pour une gestion optimisée de votre
+                                <span className="font-bold text-white text-3xl">
+                                    LA
+                                </span>{" "}
+                                solution pour une gestion optimisée de votre
                                 flotte de matériel.
                             </p>
+                            <Button className="mt-4">Tenter l'aventure</Button>
                         </div>
                     </div>
                 </section>
-                <section className="my-10" id="solution">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Doloremque temporibus laborum architecto maxime. Corporis
-                    voluptatem vel tenetur nihil sapiente! Fuga alias
-                    consequatur sed magni. Non doloribus nobis quos debitis
-                    ullam commodi ab dolorem tenetur, illo fugiat! Delectus,
-                    praesentium aliquam! Ullam eligendi tempora vel dolor
-                    doloremque odit officia pariatur cupiditate minima?
-                    Distinctio, adipisci. Sapiente itaque modi neque officia
-                    enim quo! Suscipit id exercitationem atque! Iste quaerat
-                    beatae repudiandae. Quod saepe suscipit repudiandae sapiente
-                    voluptate nostrum quos! Nulla quaerat nisi commodi
-                    doloremque consequuntur, sapiente vitae, dignissimos
-                    perspiciatis error corrupti quibusdam ratione exercitationem
-                    nesciunt iste eligendi molestiae a at distinctio! Tempore
-                    minima dolorem iusto voluptatibus recusandae ullam corrupti
-                    deserunt omnis dolorum, dolore quasi porro labore commodi,
-                    molestiae explicabo tenetur! Autem, laudantium? Vero numquam
-                    eum assumenda ipsam, labore reiciendis natus voluptate
-                    tenetur dignissimos fugiat ea delectus libero culpa dolore
-                    illum ratione corrupti at quisquam, id reprehenderit
-                    voluptatum saepe atque veritatis consequatur? Necessitatibus
-                    quaerat voluptate unde ipsa est molestiae dolore accusamus
-                    itaque aliquam sit, aperiam, aliquid ut reiciendis
-                    praesentium! Aspernatur, nihil. Nemo, inventore! Cum
-                    necessitatibus beatae sapiente saepe totam voluptatum ex,
-                    tenetur repellendus doloribus atque omnis quisquam
-                    reprehenderit accusantium nemo in vitae maxime modi
-                    perspiciatis debitis, maiores cumque culpa facere inventore.
-                    Voluptas tempora quidem rem sunt, placeat ipsa esse facere
-                    similique rerum vitae, distinctio architecto aliquid
-                    voluptatum dolore pariatur ratione nesciunt. Unde aliquam
-                    sit aut quibusdam officia fugiat ipsa omnis, et, enim quod
-                    voluptatibus laudantium consequatur reiciendis minima
-                    repellendus tempora aliquid aperiam. Dignissimos cum
-                    quisquam quidem, possimus temporibus obcaecati eligendi,
-                    esse recusandae et, pariatur amet ipsum reprehenderit saepe
-                    praesentium consequuntur ea inventore. Reprehenderit non eum
-                    aspernatur accusamus veniam repellat sequi magnam, vero sed,
-                    quas excepturi suscipit assumenda dolorum voluptates
-                    consequuntur quos unde. Nobis labore incidunt adipisci
-                    earum, officia similique temporibus dignissimos et!
-                    Obcaecati dignissimos corrupti, soluta impedit eos dicta,
-                    tempore molestias deserunt unde blanditiis veritatis
-                    exercitationem in architecto vero commodi officiis
-                    voluptatum expedita repellat perspiciatis. A quidem magnam
-                    perspiciatis, praesentium sit, nam hic vero suscipit dolores
-                    est cumque nisi inventore ratione laudantium, cum eaque.
-                    Itaque, voluptatibus placeat. Hic nulla omnis illum, quam
-                    facilis vero aspernatur atque eaque itaque quo autem, fuga
-                    cum inventore repudiandae reprehenderit dolorem assumenda
-                    doloribus fugit quis velit minus asperiores voluptatem?
-                    Mollitia sit ipsam commodi dolorum tenetur officia, dolorem
-                    doloremque sed harum minima libero, molestias corporis ad
-                    magnam quae odio cumque cum nisi adipisci sunt! Magni ipsum
-                    earum ipsam alias ea qui temporibus quos repudiandae numquam
-                    velit inventore reprehenderit aperiam quibusdam dolor,
-                    consequatur error sed officia consequuntur adipisci labore
-                    repellat unde, veritatis expedita quisquam. Odit enim et,
-                    voluptas repellat dignissimos quaerat minus aspernatur
-                    recusandae at sequi asperiores molestias non labore
-                    accusamus beatae cupiditate hic harum, libero deserunt quam
-                    vel laboriosam. Reprehenderit nam fuga voluptate rerum
-                    perspiciatis in nemo numquam saepe odit ipsa doloribus
-                    voluptatum cum modi impedit voluptates, vero incidunt nobis
-                    eveniet dolorem explicabo ipsum. Dolorum repudiandae, iure
-                    similique iusto temporibus omnis et odio itaque earum animi
-                    autem voluptatem quaerat cum accusantium dicta enim expedita
-                    ducimus, minus nulla ipsa labore cupiditate magni. Dolor
-                    reiciendis mollitia magni optio veritatis. Ex nihil ullam
-                    quas.
+                <section className="bg-white" id="solution">
+                    <div className="max-w-7xl mx-auto py-10 px-4 flex max-lg:flex-col justify-center items-center lg:justify-around">
+                        <ul className="grid max-lg:grid-cols-1 lg:grid-cols-3">
+                            <li className="">
+                                <img src={Bus} alt="" />
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aperiam quae iure officiis suscipit ad sequi, doloribus repellat ab ratione?</p>
+                            </li>
+                            <li>
+                                <img src={Truck} alt="" />
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aperiam quae iure officiis suscipit ad sequi, doloribus repellat ab ratione?</p>
+                            </li>
+                            <li>
+                                <img src={Car} alt="" />
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aperiam quae iure officiis suscipit ad sequi, doloribus repellat ab ratione?</p>
+                            </li>
+                        </ul>
+                    </div>
                 </section>
                 <section className="my-10" id="pricing">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.
